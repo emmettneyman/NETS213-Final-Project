d3fa6f7f035d823be47b23db72f598ab30078f6d
bfineran <catcher888@gmail.com>
2016-04-18 21:34:06 -0400

    Commented aggregation/ majority vote code, still needs to be modified based on final csv

--- aggregation.py
+++ aggregation.py
@@ -5,26 +5,31 @@ import csv
 
 
 def main():
+	#open data from crowd
 	fulldata = open('qcdata.csv', "r")
 	filereader = csv.reader(fulldata)
 
-	#majority vote 1
 	votes = {}
 	
+	#just filters out the headers
 	b = True
 	for row in filereader:
 		if b:
+			#skip headers
 			b = False
 			continue
+		#checks if the current question has been voted on, puts each of its votes into the list
 		if row[0] not in votes:
 			votes[row[0]] = []
 			for i in range 10:
+				#appends each value to our list in the dictionary (change the list index as needed)
 				votes[row[0]].append(row[i + 3])
 	vals = {}
+	#print the average value for each question id
 	for id in votes:
 		print(id + "\t" + avg(votes[id]))
 
-
+#airthmetic mean of a list of integers
 def avg(input):
 	sum = 0
 	for i in input:
